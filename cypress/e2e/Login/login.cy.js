fca315f73709cd57b98207d4310c1c9e85eabad3
alicia souza <alicia.rsouza@gmail.com>
2024-02-13 14:43:32 -0300

    Terminado organização com POM

--- cypress/e2e/Login/login.cy.js
+++ cypress/e2e/Login/login.cy.js
@@ -16,76 +16,64 @@ describe("Página de Login", () => {
     login.preencherLogin(Cypress.env("email"), Cypress.env("senha"));
     login.clicarBotaoAcessar();
   });
-
+  
   it("C02 - Validar falha ao tentar realizar login.", () => {
     login.preencherLogin("teste@teste.com", "senha123");
     login.clicarBotaoAcessar();
-    expect(cy.get(elemento.modal).should("be.visible"));
-    expect(
-      cy
-        .get(elemento.textoModal)
-        .should("be.visible")
-        .and(
-          "have.text",
-          `Usuário ou senha inválido.\nTente novamente ou verifique suas informações!`
-        )
+    cy.get(elemento.modal).should("be.visible");
+    cy.get(elemento.textoModal).should(
+      "be.visible",
+      "Usuário ou senha inválido.\nTente novamente ou verifique suas informações!"
     );
   });
-
+  
   context("C03 - Validar campos vazios.", () => {
     it("Campo e-mail vazio", () => {
       login.preencherSenha(Cypress.env("senha"));
       login.clicarBotaoAcessar();
-
-      expect(
-        cy
-          .get(elemento.mensagemCampoObrigatorio)
-          .should("be.visible")
-          .and("contain.text", "É campo obrigatório")
-      );
+  
+      cy.get(elemento.mensagemCampoObrigatorio)
+        .should("be.visible")
+        .and("contain.text", "É campo obrigatório");
     });
-
+  
     it("Validar campos vazios.", () => {
       login.preencherEmail("teste@teste.com");
       login.clicarBotaoAcessar();
-
-      expect(
-        cy
-          .get(elemento.mensagemCampoObrigatorio)
-          .should("be.visible")
-          .and("contain.text", "É campo obrigatório")
-      );
+  
+      cy.get(elemento.mensagemCampoObrigatorio)
+        .should("be.visible")
+        .and("contain.text", "É campo obrigatório");
     });
   });
-
+  
   it(`C04 - Validar botão "fechar" no modal em caso de falha de login.`, () => {
     login.preencherLogin("joao@hotmail.com", "senha123");
     login.clicarBotaoAcessar();
     login.clicarBotaoFecharModal();
     cy.get(elemento.modal).should("not.exist");
   });
-
+  
   it(`C05 - Validar botão "x" no modal em caso de falha de login.`, () => {
     login.preencherLogin("teste@teste.com", "senha123");
     login.clicarBotaoAcessar();
     login.clicarBotaoXModal();
     cy.get(elemento.modal).should("not.exist");
   });
-
+  
   it("C06 - Validar visualização de senha oculta.", () => {
     login.preencherSenha("senha");
     login.clicarBotaoOlhinho();
-
-    expect(
-      cy
-        .get(elemento.textoSenhaVisivel)
-        .should("be.visible")
-        .and("have.value", "senha")
-    );
+  
+    cy.get(elemento.textoSenhaVisivel)
+      .should("be.visible")
+      .and("have.value", "senha");
   });
-
+  
   it("C07 - Validar ocultar senha.", () => {
     login.preencherSenha(Cypress.env("senha"));
-    expect(cy.get(elemento.textoSenhaEscondido).should("be.visible"));
+    cy.get(elemento.textoSenhaEscondido).should("be.visible");
   });
+
+  
 });
